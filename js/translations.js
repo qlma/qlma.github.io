d723a4dececf981f17f2c7e5783c107f9c3a06a2
Jouni Leino <jouni.leino@siili.com>
2020-06-10 12:49:16 +0200

    Adding translations

---
+++ js/translations.js
@@ -0,0 +1,214 @@
+var dictionary = {
+    'title': {
+        'fi': 'QLMA - ja menee viesti perille',
+        'en': 'QLMA - and the message gets delivered',
+        'de': 'QLMA - und die Nachricht geht',
+    },
+    'lead-in': {
+        'fi': 'Koulun ja kodin välinen viestintä kaipaa parempia palveluita',
+        'en': 'Communication between school and home requires better services',
+        'de': 'Die Kommunikation zwischen Schule und Zuhause erfordert bessere Dienstleistungen',
+    },
+    'heading': {
+        'fi': 'TEHDÄÄN SELLAINEN',
+        'en': "LET'S DEVELOP IT",
+        'de': 'LASS UNS DAS MACHEN',
+    },
+    'services-section-heading': {
+        'fi': 'MITÄ TULISI PARANTAA',
+        'en': 'WHAT SHOULD BE IMPROVED',
+        'de': 'WAS SOLLTE VERBESSERT WERDEN',
+    },
+    'services-section-subheading': {
+        'fi': 'Huono käytettävyys ja vanhanaikaiset ratkaisut alkavat riittää.',
+        'en': 'Poor usability and old-fashioned solutions are starting to suffice.',
+        'de': 'Schlechte Benutzerfreundlichkeit und altmodische Lösungen beginnen zu genügen.',
+    },
+    'services-service-heading_1': {
+        'fi': 'Yleinen tyytymättömyys',
+        'en': 'General dissatisfaction',
+        'de': 'Allgemeine Unzufriedenheit',
+    },
+    'services-p_1': {
+        'fi': 'Käyttäjät eivät ole tyytyväisiä tarjolla oleviin ratkaisuihin. Ne koetaan vanhoiksi, kankeiksi ja hankalasti käytettäviksi.',
+        'en': 'Users are not satisfied with the solutions available. They are perceived as old, stiff and difficult to use.',
+        'de': 'Benutzer sind mit den verfügbaren Lösungen nicht zufrieden. Sie werden als alt, steif und schwer zu bedienen empfunden.',
+    },
+    'services-service-heading_2': {
+        'fi': 'Mobiilikäytettävyys',
+        'en': 'Mobile Usability',
+        'de': 'Mobile Benutzerfreundlichkeit',
+    },
+    'services-p_2': {
+        'fi': 'Yhä useampi haluaisi hoitaa kaiken viestinnän älypuhelimen avulla. Nykyiset palvelut vaativat vielä isoilta osin pöytätietokonetta, jota kaikilla ei ole.',
+        'en': 'More and more people would like to handle all communications using a smartphone. Existing services still require, to a large extent, a desktop computer, which not everyone has.',
+        'de': 'Immer mehr Menschen möchten die gesamte Kommunikation über ein Smartphone abwickeln. Bestehende Dienste erfordern noch weitgehend einen Desktop-Computer, über den nicht jeder verfügt.',
+    },
+    'services-service-heading_3': {
+        'fi': 'Eletään vuotta 2020',
+        'en': 'It is year 2020',
+        'de': 'Es ist das Jahr 2020',
+    },
+    'services-p_3': {
+        'fi': 'Tottumukset, vaatimukset ja odotukset ovat korkeammalla. Haluamme että viestintä on yhtä helppoa kuin nykyisten some-palveluiden hyödyntäminen.',
+        'en': 'Habits, demands and expectations are higher. We want communication to be as easy as leveraging existing some services.',
+        'de': 'Gewohnheiten, Anforderungen und Erwartungen sind höher. Wir möchten, dass die Kommunikation so einfach ist wie die Nutzung vorhandener Dienste.',
+    },
+    'portfolio-section-heading': {
+        'fi': 'Mitä sitten teemme?',
+        'en': 'So what do we do?',
+        'de': 'Also, was machen wir?',
+    },
+    'portfolio-section-subheading': {
+        'fi': 'Luonteva tapa yhdistää opettajat, huoltajat ja oppilaat.',
+        'en': 'A natural way to connect teachers, parents and students.',
+        'de': 'Ein natürlicher Weg, um Lehrer, Erziehungsberechtigte und Schüler zu verbinden.',
+    },
+    'portfolio-h4_1': {
+        'fi': 'Viestintä',
+        'en': 'Communication',
+        'de': 'Kommunikation',
+    },
+    'portfolio-p_1': {
+        'fi': 'Tehostamme viestin perillemenoa.',
+        'en': 'We improve the message delivery.',
+        'de': 'Wir verbessern die Kanäle.',
+    },
+    'portfolio-h4_2': {
+        'fi': 'Dialogi',
+        'en': 'Dialog',
+        'de': 'Dialog',
+    },
+    'portfolio-p_2': {
+        'fi': 'Helpotamme keskusteluyhteyttä.',
+        'en': 'We make the conversation easier.',
+        'de': 'Wir erleichtern das Gespräch.',
+    },
+    'portfolio-h4_3': {
+        'fi': 'Tiedottaminen',
+        'en': 'Information',
+        'de': 'Information',
+    },
+    'portfolio-p_3': {
+        'fi': 'Yksinkertaistamme tiedottamista.',
+        'en': 'We simplify communication.',
+        'de': 'Wir vereinfachen die Kommunikation.',
+    },
+    'portfolio-h4_4': {
+        'fi': 'Käytettävyys',
+        'en': 'Usability',
+        'de': 'Benutzerfreundlichkeit',
+    },
+    'portfolio-p_4': {
+        'fi': 'Rakennamme nykyistä paremman.',
+        'en': 'We are building better.',
+        'de': 'Wir bauen besser.',
+    },
+    'portfolio-h4_5': {
+        'fi': 'Nykyaikaisuus',
+        'en': 'Modernity',
+        'de': 'Modernität',
+    },
+    'portfolio-p_5': {
+        'fi': 'Palvelu joka kestää aikaa.',
+        'en': 'A service that lasts time.',
+        'de': 'Ein Service, der lange dauert.',
+    },
+    'portfolio-h4_6': {
+        'fi': 'Avoimuus',
+        'en': 'Openness',
+        'de': 'Offenheit',
+    },
+    'portfolio-p_6': {
+        'fi': 'Avoin lähdekoodi käyttöösi.',
+        'en': 'Open source for your use.',
+        'de': 'Open Source für Ihren Gebrauch.',
+    },
+    'team-heading': {
+        'fi': 'JOUKKOISTAMALLA KAIKKI MAHDOLLINEN',
+        'en': 'BY CROWD SOURCING EVERYTHING IS POSSIBLE',
+        'de': 'MIT CROWD SOURCING IST ALLES MÖGLICH',
+    },
+    'team-subheading': {
+        'fi': 'Haluamme ihmisten osallistuvan vaikuttamaan.',
+        'en': 'We want people to be involved.',
+        'de': 'Wir möchten, dass die Menschen einbezogen werden.',
+    },
+    'team-h4_1': {
+        'fi': 'TUTKIMUKSET',
+        'en': 'SURVEYS',
+        'de': 'UMFRAGEN',
+    },
+    'team-p_1': {
+        'fi': 'Annamme mahdollisuuden jokaiselle vaikuttaa siihen, mitä QLMA pitää sisällään.',
+        'en': 'We give everyone a chance to influence what QLMA contains.',
+        'de': 'Wir geben jedem die Möglichkeit zu beeinflussen, was QLMA enthält.',
+    },
+    'team-h4_2': {
+        'fi': 'KEHITYS',
+        'en': 'DEVELOPMENT',
+        'de': 'ENTWICKLUNG',
+    },
+    'team-p_2': {
+        'fi': 'Koko kehitys tapahtuu täysin avoimesti verkossa, kaikkien silmien alla.',
+        'en': 'All development takes place completely openly online, under everyone’s eyes.',
+        'de': 'Alle Entwicklungen finden völlig offen online statt, unter jedermanns Augen.',
+    },
+    'team-h4_3': {
+        'fi': 'KOMMENTIT',
+        'en': 'COMMENTS',
+        'de': 'BEMERKUNGEN',
+    },
+    'team-p_3': {
+        'fi': 'Keräämme jokaisesta versiosta kommentit ja parannamme niiden pohjalta toteutusta.',
+        'en': 'We will collect comments on each version and improve implementation based on them.',
+        'de': 'Wir werden Kommentare zu jeder Version sammeln und die Implementierung basierend darauf verbessern.',
+    },
+    'team-h4_4': {
+        'fi': 'KOKONAISUUS',
+        'en': 'Ensemble',
+        'de': 'Ensemble',
+    },
+    'team-p_4': {
+        'fi': 'Keskitymme tekemään eheän kokonaisuuden jota on helppo ja mukava käyttää.',
+        'en': 'We focus on making a whole that is easy and comfortable to use.',
+        'de': 'Wir konzentrieren uns darauf, ein Ganzes zu schaffen, das einfach und bequem zu bedienen ist.',
+    },
+    'contact-h2': {
+        'fi': 'HALUATKO MUKAAN RAKENTAMAAN KOULUMAAILMAN HIENOINTA HANKETTA?',
+        'en': 'DO YOU WANT TO PARTICIPATE THE FINEST PROJECT IN THE SCHOOL WORLD?',
+        'de': 'MÖCHTEN SIE DAS BESTE PROJEKT DER SCHULWELT BAUEN?',
+    },
+    'contact-text': {
+        'fi': 'Kaikki apu on tarpeen, sillä teemme hanketta täysin vapaaehtoisesti. Jokainen käytetty työtunti on poissa meidän jokaisen vapaa-ajasta, mutta haluamme uskoa että se kannattaa. Kun yhdessä rakennamme jotain uudenlaista, käyttäjien kanssa aidosti tehtyä palvelua, haluamme todistaa että vain tekemällä voimme saada aikaan muutoksen.',
+        'en': 'All help is needed as we do the project completely voluntarily. Every hour spent is out of our every free time, but we want to believe it’s worth it. When together we build something new, a service that is truly done with users, we want to prove that only by doing can we bring about change.',
+        'de': 'Alle Hilfe wird benötigt, da wir das Projekt völlig freiwillig durchführen. Jede verbrachte Stunde gehört zu unserer Freizeit, aber wir möchten glauben, dass es sich lohnt. Wenn wir gemeinsam etwas Neues aufbauen, einen Service, der wirklich mit Benutzern gemacht wird, wollen wir beweisen, dass wir nur dadurch Veränderungen bewirken können.',
+    },
+    'contact-support': {
+        'fi': 'QLMAa tekemässä mm. @iiuusit @awsfanboy @JanneGylling @katleena - tukemassa: @upcloud @SolitaOy',
+        'en': 'Making QLMA e.g. @iiuusit @awsfanboy @JanneGylling @katleena - supported by: @upcloud @SolitaOy',
+        'de': 'Bei der Herstellung von QLMA, z. @iiuusit @awsfanboy @JanneGylling @katleena - unterstützt von: @upcloud @SolitaOy',
+    },
+    'contact-copyright': {
+        'fi': 'Tekijänoikeudet &copy; QLMA ry<br>Rakkaudella <i class="fa fa-heart"></i> Suomesta',
+        'en': 'Copyright &copy; QLMA ry<br>With <i class="fa fa-heart"></i> from Finland',
+        'de': 'Copyright &copy; QLMA ry<br>Mit <i class="fa fa-heart"> </i> aus Finnland',
+    },
+
+};
+var langs = ['fi', 'en', 'de'];
+var current_lang = langs[1];
+
+window.setLang = function(lang) {
+    current_lang = langs[lang];
+    translate();
+}
+
+function translate() {
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+
+translate();
